6106aaffa70ed22d4700db1cbfee48c21bfca3f1
Melagas <gastonmela@gmail.com>
2022-07-25 11:37:22 -0300

    calcular el promedio de edad de personas registradas, se utilizo function

--- miarchivo.js
+++ miarchivo.js
@@ -1,11 +1,42 @@
-let nombreApellido = prompt("Ingresar su Nombre y Apellido ");
-console.log(`El nombre ingresado es ${nombreApellido}`)
-let pregunta = prompt("Le gustaria recibir novedades y promociones en su mail?")
+//Primer desafio complementario
 
-while (pregunta.toLowerCase() != "no") {
-    let mail = prompt("Ingrese su mail para recibir novedades")
-    console.log("el mail ingresado es " + mail);
-    pregunta = prompt("Desea ingresar otro mail? escriba *NO para salir")
-    console.log(`Muchas gracias por su tiempo!`)
+// let nombreApellido = prompt("Ingresar su Nombre y Apellido ");
+// console.log(`El nombre ingresado es ${nombreApellido}`)
+// let pregunta = prompt("Le gustaria recibir novedades y promociones en su mail?")
+
+// while (pregunta.toLowerCase() != "no") {
+//     let mail = prompt("Ingrese su mail para recibir novedades")
+//     console.log("el mail ingresado es " + mail);
+//     pregunta = prompt("Desea ingresar otro mail? escriba *NO para salir")
+//     console.log(`Muchas gracias por su tiempo!`)
+// }
+
+
+//Desafio 1: calcular el promedio de edad de las personas registradas en la página
+
+function calcularEdad() {
+    let nuevaEdad = parseInt(prompt("Ingrese su edad"));
+    edadUsuario = edadUsuario + nuevaEdad;
+    cantidadUsuarios++;
+    return nuevaEdad
 }
 
+function pedirNombre() {
+    let nombreUsuario = prompt("Ingrese su nombre");
+    return nombreUsuario
+}
+
+function promedioEdad() {
+    alert(`La edad promedio es ${edadUsuario / cantidadUsuarios}`);
+    console.log(`Edad promedio = ${edadUsuario / cantidadUsuarios}`)
+}
+
+let nombreUsuario = pedirNombre()
+let edadUsuario = 0;
+let cantidadUsuarios = 0;
+while (nombreUsuario.toLowerCase() != "calcular") {
+    let nuevaEdad = calcularEdad()
+    nombreUsuario = prompt("Ingrese otro nombre o CALCULAR para obtener el promedio");
+    console.log(`${nuevaEdad}`)
+}
+promedioEdad()
