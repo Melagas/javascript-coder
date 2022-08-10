c21fc75339a1a2b58650cd2e9aad77e8c5884149
Melagas <gastonmela@gmail.com>
2022-08-09 21:03:42 -0300

    se agregó eventos y se amplió DOM

--- miarchivo.js
+++ miarchivo.js
@@ -1,5 +1,43 @@
-// Array y objetos
+//Formulario de consulta
+class Datos {
+    constructor(idd, nombres, apellidos, consulta, telefono, lineas) {
+        this.idd = idd,
+            this.nombres = nombres,
+            this.apellidos = apellidos,
+            this.consulta = consulta,
+            this.telefono = telefono,
+            this.lineas = lineas
+    }
+    //Métodos
+    mostrarDatos() {
+        console.log(`${this.idd} El nombre es ${this.nombres} el apellido es ${this.apellidos}, la consulta ${this.consulta} el teléfono es: ${this.telefono} la linea consultada es: ${this.lineas}`)
+    }
+}
 
+//Instatación
+const datos1 = new Datos(1, "Gaston", "Mela", "donde se pueden ver?", 12345678, "linea flores")
+
+const dato = [datos1]
+console.log(dato)
+
+//Función de formulario de consulta
+function datosPersona() {
+    let nombresInput = document.getElementById("nombresImput")
+    let apellidosImput = document.getElementById("apellidosImput")
+    let consultaImput = document.getElementById("consultaImput")
+    let telefonoImput = document.getElementById("telefonoImput")
+    let lineasImput = document.getElementById("lineasImput")
+    let datosNuevos = new Datos(dato.length + 1, nombresInput.value, apellidosImput.value,
+        consultaImput.value, telefonoImput.value, lineasImput.value)
+    console.log(datosNuevos)
+    dato.push(datosNuevos)
+}
+
+//Botón de guardar datos de consulta
+const guardarDatosBtn = document.getElementById("guardarDatosBtn")
+guardarDatosBtn.addEventListener("click", datosPersona)
+
+//Stock de productos
 class Productos {
     constructor(id, nombre, precio, esmalte, linea, imagen) {
         this.id = id,
@@ -30,101 +68,46 @@ const stock = [producto1, producto2, producto3, producto4, producto5, producto6,
 console.log(stock)
 
 //Plantilla de Productos   
-
-let divProductos = document.getElementById("productos")
-divProductos.setAttribute("class", "productosEstilos")
-stock.forEach((producto) => {
-    let nuevoProducto = document.createElement("div")
-    nuevoProducto.innerHTML = `<article id="${producto.id}" class="cards">
+function mostrarCatalogo() {
+    let divProductos = document.getElementById("productos")
+    divProductos.setAttribute("class", "productosEstilos")
+    divProductos.innerHTML = '';
+    stock.forEach((producto) => {
+        let nuevoProducto = document.createElement("div")
+        nuevoProducto.innerHTML = `<article id="${producto.id}" class="cards">
         <h3 class="tituloCard">${producto.nombre}</h3>
         <img src="${producto.imagen}" alt="" class="imgCard">
         <div class="content">
             <p class="precioCard"><b>Precio:</b>  ${producto.precio}</p>
             <p class="lineaCard"><b>Linea:</b> ${producto.linea}</p>
             <p class="esmalteCard"><b>Esmalte:</b> ${producto.esmalte}</p>
-            <a href=""target="blank"><strong>Agregar al carrito</strong></a>
+            <button href="" class="botonCarrito">Agregar al carrito</button>
         </div>
-    </article>
-    `
-    divProductos.appendChild(nuevoProducto)
-})
+    </article>`
+        divProductos.appendChild(nuevoProducto)
+        //stock.push (nuevoProducto)
+    })
 
-//FUNCIONES
-//Funcion de agregar un producto nuevo
-function nuevoProducto() {
-    let productoIngresado = prompt(`Ingrese el nombre del producto`)
-    let precioIngresado = prompt(`Ingrese el precio del producto`)
-    let esmalteIngresado = prompt(`Ingrese el esmalte del producto`)
-    let lineaIngresada = prompt(`Ingrese la línea del producto`)
-    let productoCreado = new Productos(stock.length + 1, productoIngresado, precioIngresado, esmalteIngresado, lineaIngresada)
-    console.log(productoCreado)
-    stock.push(productoCreado)
-}
-
-//Eliminar producto
-function eliminarProducto() {
-    let idEliminar = prompt("Ingrese la id del producto a eliminar")
-    for (let productoBuscado of stock) {
-        if (productoBuscado.id == idEliminar) {
-            console.log("Este es el producto " + productoBuscado.producto)
-            stock.splice(idEliminar - 1, 1)
-            // console.log(stock)
-        }
+    //Botón de agregar productos
+    let btnAgregar = document.getElementsByClassName("botonCarrito")
+    for (let productoBoton of btnAgregar) {
+        productoBoton.addEventListener("click", () => {
+            console.log("Se agrego producto al carrito")
+        })
     }
 }
 
-//Opciones para el usuario en prompt
-function preguntarOpcion() {
-    let opcion = parseInt(prompt(`Ingrese el número de la opción que desea realizar:
-                        1 - Ver detalles de los productos.
-                        2 - Agregar un producto a nuestro stock. 
-                        3 - Eliminar un producto.
-                        4 - Sumar en el carrito de compras
-                        5 - Buscar un producto
-                        0 - Para salir
-                        `))
-    menu(opcion)
-}
+//Botón de mostrar catálogo
+const verProductos = document.getElementById("botonVerCatalogo")
+verProductos.addEventListener("click", mostrarCatalogo)
 
-//Buscador de productos por esmalte utilizado
-function buscadorDeEsmaltes() {
-    let buscador = prompt("Ingrese Duncan o Nacional para ver los productos con los esmaltes deseados.")
-    let buscarEsmalte = stock.filter((el) => el.esmalte == buscador)
-    console.log("Estos son los productos que coinciden")
-    console.log(buscarEsmalte)
-}
-
-//Menu de opciones con switch
-function menu(opcionSeleccionada) {
-    switch (opcionSeleccionada) {
-        case 0:
-            salir = true
-            alert(`Gracias por su visita! AVR Cerámica.`)
-            break
-        case 1:
-            stock.forEach((producto) => producto.mostrarProductos())
-            break
-        case 2:
-            nuevoProducto()
-            break
-        case 3:
-            eliminarProducto()
-            break
-        case 4:
-            let compra = [producto1, producto2, producto4]
-            let totalCompra = compra.reduce((acum, elemento) => acum = acum + elemento.precio, 0)
-            console.log(totalCompra)
-            break
-        case 5:
-            buscadorDeEsmaltes()
-            break
-        default:
-            alert(`Ingrese una opción correcta`)
-    }
-}
-
-//Código
-let salir
-while (salir != true) {
-    preguntarOpcion()
-}
+//Funcion de agregar un producto nuevo (proxima a modificar)
+// function nuevoProducto() {
+//     let productoIngresado = prompt(`Ingrese el nombre del producto`)
+//     let precioIngresado = prompt(`Ingrese el precio del producto`)
+//     let esmalteIngresado = prompt(`Ingrese el esmalte del producto`)
+//     let lineaIngresada = prompt(`Ingrese la línea del producto`)
+//     let productoCreado = new Productos(stock.length + 1, productoIngresado, precioIngresado, esmalteIngresado, lineaIngresada)
+//     console.log(productoCreado)
+//     stock.push(productoCreado)
+// }
